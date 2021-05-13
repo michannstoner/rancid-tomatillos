415002389589e39da29fd5ae814c2d78eb5631c7
Sarah Lane <sarahlane8@gmail.com>
2021-05-13 15:34:06 -0600

    Add find method to display movie user clicks on

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -2,7 +2,11 @@ import React from 'react'
 import './MovieDetails.css'
 
 
-const MovieDetails = () => {
+const MovieDetails = ( {movieData, id} ) => {
+  const movieToDisplay = movieData.find(movie => {
+    return movie.id === id
+  })
+  console.log(movieToDisplay)
   return (
     <h1>Hello</h1>
   )
