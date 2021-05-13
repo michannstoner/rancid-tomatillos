286fa83c685590b3a51ef2103d0ee21d8a18b445
Your Name <stoner.michann@gmail.com>
2021-05-13 17:19:55 -0600

    Added gradient and styling for background image

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import './MovieDetails.css'
 
-
 const MovieDetails = ( {movieData, id, displayAllMovies} ) => {
   const movieToDisplay = movieData.find(movie => {
     return movie.id === id
   })
+
+  const backgroundStyle = {
+    backgroundImage: `linear-gradient(to right, #1C1D1E, 60%, transparent), url(${movieToDisplay.backdrop_path})`
+  }
   return (
-    <div className="movieDetailsContainer">
-      <img src={movieToDisplay.poster_path} />
-      <h1>{movieToDisplay.title}</h1>
+    <section className="movieDetailsContainer" style={backgroundStyle}>
+      <h2>{movieToDisplay.title}</h2>
       <h2>{movieToDisplay.average_rating.toFixed(1)} ⭐️</h2>
       <button onClick={ () => displayAllMovies()}>GO BACK</button>
-    </div>
+    </section>
   )
 }
 
