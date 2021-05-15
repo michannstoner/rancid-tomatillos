efe3d66aab83e229535c3d34f441290934cfe687
Your Name <stoner.michann@gmail.com>
2021-05-14 19:22:10 -0600

    Update single movie details from fetched data

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { getSingleMovie } from '../../apiCalls'
 import './MovieDetails.css'
 
-const MovieDetails = ( {movieData, id, displayAllMovies} ) => {
-  const movieToDisplay = movieData.find(movie => {
-    return movie.id === id
-  })
-  const backgroundStyle = {
-    backgroundImage: `linear-gradient(to right, #1C1D1E, 60%, transparent), url(${movieToDisplay.backdrop_path})`
-  }
-  const movieDate = new Date(`${movieToDisplay.release_date}`)
+
+const MovieDetails = ( { singleMovieDetails, displayAllMovies} ) => {
+  const movieDate = new Date(`${singleMovieDetails.movie.release_date}`)
   const yearReleased = movieDate.getFullYear()
+
+  const backgroundStyle = {
+    backgroundImage: `linear-gradient(to right, #1C1D1E, 60%, transparent), url(${singleMovieDetails.movie.backdrop_path})`
+  }
 
   return (
     <section className="movieDetailsContainer" style={backgroundStyle}>
-      <h2 className='movieTitle'>{movieToDisplay.title}</h2>
+      <h2 className='movieTitle'>{singleMovieDetails.movie.title}</h2>
       <div className='infoContainer'>
-        <h2>⭐️ {movieToDisplay.average_rating.toFixed(1)}</h2>
+        <h2>⭐️ {singleMovieDetails.movie.average_rating.toFixed(1)}</h2>
         <h2>{yearReleased}</h2>
       </div>
       <button onClick={ () => displayAllMovies()}>GO BACK</button>
     </section>
-  )
+  )  
 }
 
 export default MovieDetails
