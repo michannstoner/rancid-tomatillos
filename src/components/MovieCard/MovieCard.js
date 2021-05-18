a4f3b27cba72c27fead05e4a511555d3f5139098
Your Name <stoner.michann@gmail.com>
2021-05-18 15:09:06 -0600

    Put button back and attach Link

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './MovieCard.css'
+import { Link } from 'react-router-dom'
 
 const MovieCard = props => {
   return (
     <article className='movieCard'>
       <img src={props.img} alt="movie poster"/>
-      <button onClick={ () => props.displayMovieDetails(props.id)}>Click for Details</button>
+      <Link to={`/movies/${props.id}`}>
+        <button onClick={ () => props.displayMovieDetails(props.id)}>Click for Details</button>
+      </Link>
       <p>{props.title}</p>
+
     </article>
   )
 }
