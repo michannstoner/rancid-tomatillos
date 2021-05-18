16cd41fd60c7545cc221119fc54b7dfe811d4c3f
Your Name <stoner.michann@gmail.com>
2021-05-18 17:16:21 -0600

    Remove button and make image clickable

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,9 +5,8 @@ import { Link } from 'react-router-dom'
 const MovieCard = props => {
   return (
     <article className='movieCard'>
-      <img src={props.img} alt="movie poster"/>
       <Link to={`/movies/${props.id}`}>
-        <button>Click for Details</button>
+        <img src={props.img} alt="movie poster"/>
       </Link>
       <p>{props.title}</p>
 
