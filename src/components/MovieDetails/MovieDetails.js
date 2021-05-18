3377125aa290cf13d944cb23393afb2ed485c8db
Your Name <stoner.michann@gmail.com>
2021-05-18 15:18:22 -0600

    Change MovieDetails to class Component

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -2,30 +2,42 @@ import React, { Component } from 'react'
 import { getSingleMovie } from '../../apiCalls'
 import './MovieDetails.css'
 
-
-const MovieDetails = ( { singleMovieDetails, displayAllMovies} ) => {
-  const movieDate = new Date(`${singleMovieDetails.movie.release_date}`)
-  const yearReleased = movieDate.getFullYear()
-
-  const backgroundStyle = {
-    backgroundImage: `linear-gradient(to right, #1C1D1E, 60%, transparent), url(${singleMovieDetails.movie.backdrop_path})`
+class MovieDetails extends Component {
+  constructor() {
+    super()
+    this.state = {
+      singleMovie: {}
+    }
   }
 
-  return (
-    <section className="movieDetailsContainer" style={backgroundStyle}>
-      <h2 className='movieTitle'>{singleMovieDetails.movie.title}</h2>
-      <div className='infoContainer'>
-        <h2>⭐️ {singleMovieDetails.movie.average_rating.toFixed(1)}</h2>
-        <h2>{yearReleased}</h2>
-      </div>
-      <div className='overviewContainer'>
-        <p>{singleMovieDetails.movie.overview}</p>
-      </div>
-      <div className='buttonContainer'>
-        <button onClick={ () => displayAllMovies()}>GO BACK</button>
-      </div>
-    </section>
-  )  
+  componentDidMount = ({ props.id }) => {
+    getSingleMovie()
+
+  }
+
+  render() {
+    const movieDate = new Date(`${singleMovieDetails.movie.release_date}`)
+    const yearReleased = movieDate.getFullYear()
+
+    const backgroundStyle = {
+    backgroundImage: `linear-gradient(to right, #1C1D1E, 60%, transparent), url(${singleMovieDetails.movie.backdrop_path})`
+  }
+    return (
+      <section className="movieDetailsContainer" style={backgroundStyle}>
+        <h2 className='movieTitle'>{singleMovieDetails.movie.title}</h2>
+        <div className='infoContainer'>
+          <h2>⭐️ {singleMovieDetails.movie.average_rating.toFixed(1)}</h2>
+          <h2>{yearReleased}</h2>
+        </div>
+        <div className='overviewContainer'>
+          <p>{singleMovieDetails.movie.overview}</p>
+        </div>
+        <div className='buttonContainer'>
+          <button onClick={ () => displayAllMovies()}>GO BACK</button>
+        </div>
+      </section>
+    )  
+  }
 }
 
 export default MovieDetails
