e39b61a60c78e9d8e2e23f204e05b801e049c3d9
Your Name <stoner.michann@gmail.com>
2021-05-18 20:11:42 -0600

    Create method to fetch a video

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,4 +8,9 @@ const getSingleMovie = id => {
     .then(response => response.json())
 }
 
-export { getAllMovies, getSingleMovie }
+const getSingleVideo = id => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)
+    .then(response => response.json())
+}
+
+export { getAllMovies, getSingleMovie, getSingleVideo }
