622cbdba7c612332e9cf53f328d8df3a9d15f526
Your Name <stoner.michann@gmail.com>
2021-05-19 16:52:48 -0600

    Write test for input value on form

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -6,5 +6,16 @@ describe('Homepage', () => {
 
   it('should display a nav bar', () => {
     cy.get('h1').contains('Rancid Tomatillos')
+      .get('form > input')
+      .get('button').contains('CLEAR')
+  })
+
+  it('should show the input value as the user is typing', () => {
+    cy.get('form > input').type('mulan')
+      .should('have.value', 'mulan')
+  })
+
+  it('should display movie cards on page load', () => {
+    
   })
 })
