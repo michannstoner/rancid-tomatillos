3bba92eea35afbf42b7b898d6efa779ec11ce7b8
Your Name <stoner.michann@gmail.com>
2021-05-18 20:12:00 -0600

    Render video in MovieDetails

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { getSingleMovie } from '../../apiCalls'
+import { getSingleMovie, getSingleVideo } from '../../apiCalls'
 import './MovieDetails.css'
 import { Link } from 'react-router-dom'
 import { filterSingleMovieResult } from '../../utilities'
@@ -9,17 +9,24 @@ class MovieDetails extends Component {
     super(props);
     this.state = {
       singleMovie: {},
+      singleVideo: {},
       error: "",
-    };
+    }
   }
-  componentDidMount = () => {
+  componentDidMount = (id) => {
     getSingleMovie(this.props.movieId)
       .then((data) => {
         const filteredMovieData = filterSingleMovieResult(data)
-        this.setState({ singleMovie: filteredMovieData });
+        this.setState({ singleMovie: filteredMovieData })
       })
       .catch((error) => this.setState({ error: "Something went wrong!" }));
-  };
+    
+      getSingleVideo(this.props.movieId)
+      .then(data => {
+        this.setState({ singleVideo: data.videos[0]})
+        console.log(this.state.singleVideo);
+      })
+  }
 
   render() {
     const movieDate = new Date(`${this.state.singleMovie.release_date}`)
@@ -27,7 +34,6 @@ class MovieDetails extends Component {
     const backgroundStyle = {
     backgroundImage: `linear-gradient(to right, #1C1D1E, 60%, transparent), url(${this.state.singleMovie.backdrop_path})`
     }
-
     return (
       <section className="movieDetailsContainer" style={backgroundStyle}>
         <h2 className='movieTitle'>{this.state.singleMovie.title}</h2>
@@ -38,6 +44,15 @@ class MovieDetails extends Component {
         </div>
         <div className='overviewContainer'>
           <p>{this.state.singleMovie.overview}</p>
+        </div>
+        <div className='videoContainer'>
+        <iframe 
+          src={`https://youtube.com/embed/${this. state.singleVideo.key}`}
+          alt='trailer-iframe-video-player'
+          title='trailer-video-player'
+          height='400'
+          width='700'>
+        </iframe>
         </div>
         <Link to='/'>
           <div className='buttonContainer'>
