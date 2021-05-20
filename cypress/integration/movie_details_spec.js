e2c1f3482df3537f6d76f60dc8b6a5ca592d43ec
Your Name <stoner.michann@gmail.com>
2021-05-19 19:39:06 -0600

    Write tests for movie details component

---
+++ cypress/integration/movie_details_spec.js
@@ -0,0 +1,45 @@
+describe('Movie Details', () => {
+  it('should display a movie\'s details', () => {
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      'movies': [
+        {
+          'id': 12367,
+          'poster_path': "https://image.tmdb.org/t/p/original//dqA2FCzz4OMmXLitKopzf476RVB.jpg",
+          'title': 'Another One',
+          'average_rating': 100
+        },
+        {
+          'id': 12345,
+          'poster_path': "https://image.tmdb.org/t/p/original//tEvGSlRO0dz1pINzjSEELdTHvwk.jpg",
+          'title': 'A Movie',
+          'average_rating': 100
+        }
+      ]    
+    })
+      .intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/12367', {
+        'movie':  {
+          "id": 12367,
+          "title": 'Another One',
+          "backdrop_path": "https://image.tmdb.org/t/p/original//6j2g4fEAmjJqAclzprIpvz9Gaw4.jpg",
+          "release_date": '2018-03-01',
+          "overview": 'Some overview',
+          "average_rating": 100
+        }
+    })
+      .intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/12367/videos', {
+        'videos': [
+          {
+            "key": "yMvJSivVNrs"
+          }
+        ]
+      })
+      .visit('http://localhost:3000')
+      .get('.movieCard:first').click()
+      .url().should('include', '12367')
+      .get('.movieTitle').contains('Another One')
+      .get('.movieDetailsContainer').should('be.visible')
+      .get('iframe').should('be.visible')
+      .get('.infoContainer').contains('⭐️')
+
+  })
+})
