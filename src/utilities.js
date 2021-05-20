b9bffe58a8fe8740bde97ef7dfdaadeb97ddb165
Your Name <stoner.michann@gmail.com>
2021-05-20 16:58:14 -0600

    Change double quotes to single

--- src/utilities.js
+++ src/utilities.js
@@ -1,10 +1,10 @@
 const filterMoviesResults = moviesData => {
   const filteredData = moviesData.movies.map(movie => {
     return {
-      "id": movie.id,
-      "poster_path": movie.poster_path,
-      "title": movie.title,
-      "average_rating": movie.average_rating
+      'id': movie.id,
+      'poster_path': movie.poster_path,
+      'title': movie.title,
+      'average_rating': movie.average_rating
     }
   })
   return filteredData
@@ -13,12 +13,12 @@ const filterMoviesResults = moviesData => {
 const filterSingleMovieResult = singleMovieData => {
   const { id, title, backdrop_path, release_date, overview, average_rating } = singleMovieData.movie
   const filteredData = {
-    "id": id,
-    "title": title,
-    "backdrop_path": backdrop_path,
-    "release_date": release_date,
-    "overview": overview,
-    "average_rating": average_rating
+    'id': id,
+    'title': title,
+    'backdrop_path': backdrop_path,
+    'release_date': release_date,
+    'overview': overview,
+    'average_rating': average_rating
   }
   return filteredData
 }
