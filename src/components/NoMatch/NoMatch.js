5f80670f0bb6f1acba67e3567bde7f3854fc8636
Sarah Lane <sarahlane8@gmail.com>
2021-05-21 14:22:36 -0600

    Delete unused function on button

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 const NoMatch = () => {
   return (
     <div>
-    <h1>Sorry, page not found!</h1>
-    <Link to='/'>
-      <div className='goHomeButton'>
-        <button>GO TO HOMEPAGE</button>
-      </div>
-    </Link>
+      <h1>Sorry, page not found!</h1>
+      <Link to='/'>
+        <div className='goHomeButton'>
+          <button>GO TO HOMEPAGE</button>
+        </div>
+      </Link>
     </div>
   )
 }
